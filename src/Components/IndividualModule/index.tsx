ad4a15c94705b75eb913cde3aaf06ec27f350523
Artur Watanabe <artur.watanabe@mactek.net>
2024-07-25 12:33:54 -0300

    add: individual module page

--- src/Components/IndividualModule/index.tsx
+++ src/Components/IndividualModule/index.tsx
@@ -7,6 +7,7 @@ import Footer from "../Footer";
 import NavBar from "../NavBar";
 import FixedNavBar from "../FixedNavBar";
 import YoungWoman from "../../assets/young-pretty-busy-woman-sitting-alone-conference-room-many-yellow-chairs-scaled.jpg"
+import FolderFiles from "../../assets/undraw_folder_files_re_2cbm.svg"
 
 
 
@@ -47,27 +48,38 @@ export default function IndividualModule() {
         <Flex w="100vw" h="100%" position={"relative"} alignItems={"center"} justifyContent={"center"} flexDir={"column"}>
                 <div style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", width: "100%", minHeight: "70vh", height:"100%", position: "absolute", zIndex: 90 }} />
                 <Image position={"absolute"} src={MainImg} w="100vw" minH="70vh" h="100%" backgroundRepeat={"no-repeat"} objectFit={"cover"} objectPosition={"50% 42%"} />
-            <Flex zIndex={900} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
+            <Flex zIndex={400} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
                 <NavBar openBurger={openBurger} setOpenBurger={setOpenBurger}/>
                 <FixedNavBar setOpenBurger={setOpenBurger} dismont={scrollPosition == 0 ? true : false} render={scrollPosition >= 10 ? true : false} />
                 <Flex maxW="1260px" w="100%" position="relative" justifyContent={"center"} flexDirection={"column"}>
                 {module ? <>
-                    <Flex textAlign={"left"}  zIndex={900} p="120px 0 0 0" >
+                    <Flex textAlign={"left"} w="80%"  zIndex={900} p="120px 0 0 0" >
                     <Flex flexDir={"column"} gap="40px">
-                        <Heading color={"#fbc431"} w="100%" textAlign={"center"}>{module.name}</Heading>
-                        <Text color={"rgba(255,255,255,0.8)"}>{module.description}</Text>
+                        <Heading color={"#fbc431"} w="100%" fontWeight={400} fontSize={"46px"} textAlign={"start"}>{module.name}</Heading>
+                        <Text color={"rgba(255,255,255,0.8)"} fontSize={'22px'}>{module.description}</Text>
                     </Flex>
                 </Flex>
-                </> :   <Flex w="40%" zIndex={900} p="120px 0 0 0" ><Text fontSize={25} color="white" textAlign={"center"}>Módulo nao encontrado, tente novamente mais tarde</Text></Flex>} 
+                </> :   <Flex w="40%" zIndex={900} p="120px 0 0 0" ><Text fontSize={22} color="white" textAlign={"center"}>Módulo nao encontrado, tente novamente mais tarde</Text></Flex>} 
                 </Flex>
              </Flex>
             </Flex>
         <Flex w="100vw" h="100%" position={"relative"} alignItems={"center"} justifyContent={"center"} flexDir={"column"}>
         <div style={{ backgroundColor: "rgb(255, 255, 255, 0.9)", width: "100%", minHeight: "60vh", height: "100%", position: "absolute", zIndex: 90 }} />
         <Image position={"absolute"} src={YoungWoman} w="100%" minH="60vh" h="100%" backgroundRepeat={"no-repeat"} objectFit={"cover"} objectPosition={"50% 42%"} />
-            <Flex zIndex={900} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
-                  <Flex>
-                    
+            <Flex zIndex={400} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
+                  <Flex p="64px 0 0 0 " w="100%" flexDir={"column"}>
+                      <Heading fontSize={"46px"} textAlign={"center"} fontWeight={400}color="#FFB800">Vantagens</Heading>
+                      <Flex w="100%" mt="64px" border={"1px solid red"} alignItems={{sm:"center", lg:"start"}} gap={{sm:"15px", lg: "0px"}} justifyContent={"space-around"} flexDir={{sm:"column", lg:"row"}}>
+                        <Flex w={{sm: "80%", lg:"290px"}} h="390px" flexDir={"column"} alignItems={"center"} p="15px 0 0 0" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
+                            <Image src={FolderFiles} w="80%"/>
+                        </Flex>
+                        <Flex w={{sm: "80%", lg:"290px"}} h="390px" flexDir={"column"} alignItems={"center"} p="15px 0 0 0" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
+                            <Image src={FolderFiles} w="80%"/>
+                        </Flex>
+                        <Flex w={{sm: "80%", lg:"290px"}} h="390px" flexDir={"column"} alignItems={"center"} p="15px 0 0 0" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
+                            <Image src={FolderFiles} w="80%"/>
+                        </Flex>
+                      </Flex>
                   </Flex>
              </Flex>
             </Flex>
