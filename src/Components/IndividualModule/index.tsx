8c28c51e4271ebc87fdb762f3044af8d6da11019
Artur Watanabe <artur.watanabe@mactek.net>
2024-07-25 13:29:18 -0300

    feat: individual module components

--- src/Components/IndividualModule/index.tsx
+++ src/Components/IndividualModule/index.tsx
@@ -45,21 +45,21 @@ export default function IndividualModule() {
         <Flex flexDir={"column"}>
 
 
-        <Flex w="100vw" h="100%" position={"relative"} alignItems={"center"} justifyContent={"center"} flexDir={"column"}>
+        <Flex w="100vw" h="100%" position={"relative"} alignItems={"center"} justifyContent={"center"} flexDir={"column"} >
                 <div style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", width: "100%", minHeight: "70vh", height:"100%", position: "absolute", zIndex: 90 }} />
                 <Image position={"absolute"} src={MainImg} w="100vw" minH="70vh" h="100%" backgroundRepeat={"no-repeat"} objectFit={"cover"} objectPosition={"50% 42%"} />
-            <Flex zIndex={400} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
+            <Flex zIndex={500} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
                 <NavBar openBurger={openBurger} setOpenBurger={setOpenBurger}/>
                 <FixedNavBar setOpenBurger={setOpenBurger} dismont={scrollPosition == 0 ? true : false} render={scrollPosition >= 10 ? true : false} />
                 <Flex maxW="1260px" w="100%" position="relative" justifyContent={"center"} alignItems={{sm: "center", lg: "start"}} flexDirection={"column"}>
                 {module ? <>
-                    <Flex textAlign={"left"} w={{sm: "90%", lg:"80%"}}   zIndex={900} p={{sm: "60px 0 0 0", lg:"120px 0 0 0"}} >
+                    <Flex textAlign={"left"} w={{sm: "90%", lg:"80%"}}   zIndex={500} p={{sm: "60px 0 0 0", lg:"120px 0 0 0"}} >
                     <Flex flexDir={"column"} gap="40px">
-                        <Heading color={"#fbc431"} w="100%" textAlign={{sm: "center", lg: "start"}}  fontWeight={400} fontSize={"46px"} >{module.name}</Heading>
+                        <Heading color={"#fbc431"} w="100%" textAlign={{sm: "center", lg: "start"}}  fontWeight={400} fontSize={{sm: "42px", lg:"46px"}} >{module.name}</Heading>
                         <Text color={"rgba(255,255,255,0.8)"} fontSize={'18px'}>{module.description}</Text>
                     </Flex>
                 </Flex>
-                </> :   <Flex w="40%" zIndex={900} p="120px 0 0 0" ><Text fontSize={22} color="white" textAlign={"center"}>Módulo nao encontrado, tente novamente mais tarde</Text></Flex>} 
+                </> :   <Flex w="40%" zIndex={500} p="120px 0 0 0" ><Text fontSize={22} color="white" textAlign={"center"}>Módulo nao encontrado, tente novamente mais tarde</Text></Flex>} 
                 </Flex>
              </Flex>
             </Flex>
@@ -89,11 +89,11 @@ export default function IndividualModule() {
                   </Flex>
              </Flex>
             </Flex>
-            <Flex w="100%" scrollMarginTop={"127px"} id="fifthSection" minH="70vh" h="100%" zIndex={500} justifyContent={"center"} position={"relative"}>
+            <Flex w="100%" scrollMarginTop={"127px"} id="fifthSection" minH="70vh" h="100%" zIndex={300} justifyContent={"center"} position={"relative"}>
           <div style={{ backgroundColor: "rgba(0, 0, 0, 0.6)", width: "100%", minHeight: "70vh", height: "100%", position: "absolute", zIndex: 90 }} />
           <Image position={"absolute"} src={YoungWoman} w="100vw" height={"100%"} minH={"70vh"} backgroundRepeat={"no-repeat"} objectFit={"cover"} objectPosition={"50% 50%"} />
           <Flex maxW="1260px" w="100%" justifyContent={"center"} alignItems={"center"} flexDirection={"column"}>
-            <Flex zIndex={900} w={{ sm: "90%", lg: "80%" }} alignItems={"center"} flexDir={"column"} gap="46px" p="110px 0" >
+            <Flex zIndex={500} w={{ sm: "90%", lg: "80%" }} alignItems={"center"} flexDir={"column"} gap="46px" p="110px 0" >
               <Heading fontSize={"36px"} fontWeight={400} color={"rgba(255,255,255,0.8)"}>Mantenha contato com nossa equipe</Heading>
               <motion.form style={{ display: "flex", gap: "15px", flexDirection: "column", width: "100%", alignItems: "center" }}>
                 <Input background={"rgba(255, 255, 255, 0.2)"} color={"rgba(255,255,255,0.8)"} placeholder="Your Name (required)" />
