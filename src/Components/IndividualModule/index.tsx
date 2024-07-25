ac8c8011773165443ec3fc737b9d08a55a7108c7
Artur Watanabe <artur.watanabe@mactek.net>
2024-07-25 13:18:54 -0300

    feat: individual module components

--- src/Components/IndividualModule/index.tsx
+++ src/Components/IndividualModule/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Image, Input, Text } from "@chakra-ui/react";
 import MainImg from "../../assets/main-demo1.jpg"
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -8,7 +8,7 @@ import NavBar from "../NavBar";
 import FixedNavBar from "../FixedNavBar";
 import YoungWoman from "../../assets/young-pretty-busy-woman-sitting-alone-conference-room-many-yellow-chairs-scaled.jpg"
 import FolderFiles from "../../assets/undraw_folder_files_re_2cbm.svg"
-
+import {motion} from "framer-motion"
 
 
 
@@ -51,12 +51,12 @@ export default function IndividualModule() {
             <Flex zIndex={400} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
                 <NavBar openBurger={openBurger} setOpenBurger={setOpenBurger}/>
                 <FixedNavBar setOpenBurger={setOpenBurger} dismont={scrollPosition == 0 ? true : false} render={scrollPosition >= 10 ? true : false} />
-                <Flex maxW="1260px" w="100%" position="relative" justifyContent={"center"} flexDirection={"column"}>
+                <Flex maxW="1260px" w="100%" position="relative" justifyContent={"center"} alignItems={{sm: "center", lg: "start"}} flexDirection={"column"}>
                 {module ? <>
-                    <Flex textAlign={"left"} w="80%"  zIndex={900} p="120px 0 0 0" >
+                    <Flex textAlign={"left"} w={{sm: "90%", lg:"80%"}}   zIndex={900} p={{sm: "60px 0 0 0", lg:"120px 0 0 0"}} >
                     <Flex flexDir={"column"} gap="40px">
-                        <Heading color={"#fbc431"} w="100%" fontWeight={400} fontSize={"46px"} textAlign={"start"}>{module.name}</Heading>
-                        <Text color={"rgba(255,255,255,0.8)"} fontSize={'22px'}>{module.description}</Text>
+                        <Heading color={"#fbc431"} w="100%" textAlign={{sm: "center", lg: "start"}}  fontWeight={400} fontSize={"46px"} >{module.name}</Heading>
+                        <Text color={"rgba(255,255,255,0.8)"} fontSize={'18px'}>{module.description}</Text>
                     </Flex>
                 </Flex>
                 </> :   <Flex w="40%" zIndex={900} p="120px 0 0 0" ><Text fontSize={22} color="white" textAlign={"center"}>Módulo nao encontrado, tente novamente mais tarde</Text></Flex>} 
@@ -66,23 +66,44 @@ export default function IndividualModule() {
         <Flex w="100vw" h="100%" position={"relative"} alignItems={"center"} justifyContent={"center"} flexDir={"column"}>
         <div style={{ backgroundColor: "rgb(255, 255, 255, 0.9)", width: "100%", minHeight: "60vh", height: "100%", position: "absolute", zIndex: 90 }} />
         <Image position={"absolute"} src={YoungWoman} w="100%" minH="60vh" h="100%" backgroundRepeat={"no-repeat"} objectFit={"cover"} objectPosition={"50% 42%"} />
-            <Flex zIndex={400} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
-                  <Flex p="64px 0 0 0 " w="100%" flexDir={"column"}>
-                      <Heading fontSize={"46px"} textAlign={"center"} fontWeight={400}color="#FFB800">Vantagens</Heading>
-                      <Flex w="100%" mt="64px" border={"1px solid red"} alignItems={{sm:"center", lg:"start"}} gap={{sm:"15px", lg: "0px"}} justifyContent={"space-around"} flexDir={{sm:"column", lg:"row"}}>
-                        <Flex w={{sm: "80%", lg:"290px"}} h="390px" flexDir={"column"} alignItems={"center"} p="15px 0 0 0" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
-                            <Image src={FolderFiles} w="80%"/>
+            <Flex zIndex={300} w="100%" maxW="1280px" pb="50px " h="100%" minH="70vh" alignItems={"center"} flexDirection={"column"} >
+                  <Flex p="64px 0 0 0 " w="100%"  flexDir={"column"}>
+                      <Heading fontSize={"42px"} textAlign={"center"} fontWeight={400}color="#FFB800">Vantagens</Heading>
+                      <Flex w="100%" mt="64px"  alignItems={{sm:"center", lg:"start"}} gap={{sm:"15px", lg: "0px"}} justifyContent={"space-around"} flexDir={{sm:"column", lg:"row"}}>
+                        <Flex w={{sm: "90%", lg:"290px"}} background="rgb(255, 255, 255, 0.9)" h={{sm:"100%", lg:"390px"}}  gap="20px" flexDir={"column"} alignItems={"center"} p="25px 20px 10px 20px" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
+                            <Image src={FolderFiles} w={{sm: "30%", lg:"60%"}}/>
+                            <Text fontSize={"26px"} fontWeight={300}>Sub-Title</Text>
+                            <Text fontSize={"16px"} fontWeight={200} textAlign={"center"}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stan</Text>
                         </Flex>
-                        <Flex w={{sm: "80%", lg:"290px"}} h="390px" flexDir={"column"} alignItems={"center"} p="15px 0 0 0" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
-                            <Image src={FolderFiles} w="80%"/>
+                        <Flex w={{sm: "90%", lg:"290px"}} h={{sm:"100%", lg:"390px"}} background="rgb(255, 255, 255, 0.9)" gap="20px" flexDir={"column"} alignItems={"center"} p="25px 20px 10px 20px" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
+                            <Image src={FolderFiles} w={{sm: "30%", lg:"60%"}}/>
+                            <Text fontSize={"26px"} fontWeight={300}>Sub-Title</Text>
+                            <Text fontSize={"16px"} fontWeight={200} textAlign={"center"}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stan</Text>
                         </Flex>
-                        <Flex w={{sm: "80%", lg:"290px"}} h="390px" flexDir={"column"} alignItems={"center"} p="15px 0 0 0" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
-                            <Image src={FolderFiles} w="80%"/>
+                        <Flex w={{sm: "90%", lg:"290px"}} h={{sm:"100%", lg:"390px"}}  background="rgb(255, 255, 255, 0.9)"gap="20px"  flexDir={"column"} alignItems={"center"} p="25px 20px 10px 20px" borderRadius={"5px"} boxShadow={"1px 1px 3px #222"}>
+                            <Image src={FolderFiles} w={{sm: "30%", lg:"60%"}}/>
+                            <Text fontSize={"26px"} fontWeight={300}>Sub-Title</Text>
+                            <Text fontSize={"16px"} fontWeight={200} textAlign={"center"}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stan</Text>
                         </Flex>
                       </Flex>
                   </Flex>
              </Flex>
             </Flex>
+            <Flex w="100%" scrollMarginTop={"127px"} id="fifthSection" minH="70vh" h="100%" zIndex={500} justifyContent={"center"} position={"relative"}>
+          <div style={{ backgroundColor: "rgba(0, 0, 0, 0.6)", width: "100%", minHeight: "70vh", height: "100%", position: "absolute", zIndex: 90 }} />
+          <Image position={"absolute"} src={YoungWoman} w="100vw" height={"100%"} minH={"70vh"} backgroundRepeat={"no-repeat"} objectFit={"cover"} objectPosition={"50% 50%"} />
+          <Flex maxW="1260px" w="100%" justifyContent={"center"} alignItems={"center"} flexDirection={"column"}>
+            <Flex zIndex={900} w={{ sm: "90%", lg: "80%" }} alignItems={"center"} flexDir={"column"} gap="46px" p="110px 0" >
+              <Heading fontSize={"36px"} fontWeight={400} color={"rgba(255,255,255,0.8)"}>Mantenha contato com nossa equipe</Heading>
+              <motion.form style={{ display: "flex", gap: "15px", flexDirection: "column", width: "100%", alignItems: "center" }}>
+                <Input background={"rgba(255, 255, 255, 0.2)"} color={"rgba(255,255,255,0.8)"} placeholder="Your Name (required)" />
+                <Input background={"rgba(255, 255, 255, 0.2)"} color={"rgba(255,255,255,0.8)"} placeholder="Your Name (required)" />
+                <Input background={"rgba(255, 255, 255, 0.2)"} color={"rgba(255,255,255,0.8)"} height={"120px"} />
+                <Button fontSize={"15px"} h="40px" w={{ sm: "55%", lg: "15%" }} fontWeight={500} _hover={{ color: "rgba(255,255,255,0.8)", background: "#C89C27" }} p={"0 18px"} background={"#FBC431"} color={"rgba(255,255,255,0.8)"}>Submit</Button>
+              </motion.form>
+            </Flex>
+          </Flex>
+        </Flex>
        <Footer/>
         </Flex>
            
